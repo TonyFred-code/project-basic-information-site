bdb37c203db144285b39386a76658491bdd2d5ee
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2025-12-06 20:20:11 +0100

    refactor: add error handling when listening to port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import { join, dirname } from 'path';
-import { fileURLToPath } from 'url';
+import dotenv from "dotenv";
+import express from "express";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
@@ -10,23 +10,27 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "public", "index.html"));
 });
 
-app.get('/about', (req, res) => {
-  res.sendFile(join(__dirname, 'public', 'about.html'));
+app.get("/about", (req, res) => {
+  res.sendFile(join(__dirname, "public", "about.html"));
 });
 
-app.get('/contact-me', (req, res) => {
-  res.sendFile(join(__dirname, 'public', 'contact-me.html'));
+app.get("/contact-me", (req, res) => {
+  res.sendFile(join(__dirname, "public", "contact-me.html"));
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(join(__dirname, 'public', '404.html'));
+app.get("*", (req, res) => {
+  res.sendFile(join(__dirname, "public", "404.html"));
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+
   console.log(`Server running on port ${PORT}`);
 });
