8882c2f48c9d7e109902c74bcb0b498ee9ba8f66
Alfred Faith <111481054+TonyFred-code@users.noreply.github.com>
2025-12-07 03:40:40 +0100

    style: format file for consistency

--- app.js
+++ app.js
@@ -27,9 +27,11 @@ app.get("*", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-}).on('error', (error) => {
-  console.error(`Server failed to start: `, error);
-  process.exit(1);
-})
+app
+  .listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  })
+  .on("error", (error) => {
+    console.error(`Server failed to start: `, error);
+    process.exit(1);
+  });
